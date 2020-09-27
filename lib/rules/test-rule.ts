26b56158e44aed3aa4694daee27f5685819cf495
Niranjan Jayakar <nija@amazon.com>
2020-09-27 11:45:37 +0100

    importdeclaration

--- lib/rules/test-rule.ts
+++ lib/rules/test-rule.ts
@@ -1,22 +1,39 @@
 import { Rule } from 'eslint';
 
-export function create(context: Rule.RuleContext) {
+export function create(context: Rule.RuleContext): Rule.NodeListener {
   return {
     // `node` is a type from @typescript-eslint/typescript-estree, but using 'any' for now
-    // since compatibility with eslint is still experimental and is hard to get them to work together
-    // Meanwhile, use https://astexplorer.net/ with parser set to 'babel-eslint' and transfrom set to
-    // 'ESLint v4' to explore the AST.
+    // since it's incompatible with eslint.Rule namespace. Waiting for better compatibility in
+    // https://github.com/typescript-eslint/typescript-eslint/tree/1765a178e456b152bd48192eb5db7e8541e2adf2/packages/experimental-utils#note
+    // Meanwhile, use https://astexplorer.net/ with parser set to '@typescript-eslint/parser' and
+    // transfrom set to 'ESLint v4' to explore the AST.
+
+    ImportDeclaration(node: any) {
+      if (node.source.value === '@aws-cdk/core') {
+        node.specifiers.forEach((s: any) => {
+          if (s.imported.name === 'Construct') {
+            context.report({
+              node,
+              message: `found import ${s.local.name}`
+            });
+          }
+        });
+      }
+    },
+
     MethodDefinition(node: any) {
       const methodName = node.key.name;
       const className = node.parent.parent.type === 'ClassDeclaration' ? node.parent.parent : undefined;
       const isConstructor = methodName === 'constructor' && className !== undefined;
 
       if (isConstructor) {
-        context.report({
-          node,
-          message: 'constructor found'
+        node.value.params.forEach((p: any) => {
+          context.report({
+            node,
+            message: `constructor types ${p.typeAnnotation.typeAnnotation.typeName.name}`
+          });
         });
       }
-    }
+    },
   }
 }
